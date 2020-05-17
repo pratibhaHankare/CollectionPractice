26140c82197131b63ef0e6bff40dd2f9871927c1
pratibha_hankare <pratibha1hankare@gmail.com>
2020-05-17 12:50:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            bool result = obj.comapre<float>(22.6f,22.7f);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+        public bool comapre<T>(T value1,T value2)
+        {
+            if (value1.Equals(value2))
+                return true;
+            return false;
+        }
+    }
+}
